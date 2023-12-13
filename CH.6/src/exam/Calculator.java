7773aff4ddd3f6e70ad773e58726cb209cdbd557
yejin1o <148242898+yejin1o@users.noreply.github.com>
2023-12-13 15:35:38 +0900

    Create Calculator.java

---
+++ CH.6/src/exam/Calculator.java
@@ -0,0 +1,19 @@
+package exam3;
+
+public class Calculator {
+	//static double pi = 3.14;
+	String color;
+	
+	void SetColor(String color) {
+		this.color = color;
+	}
+	
+	static int plus(int a, int b) {
+		//color = "Blue";
+		return a+b;
+	}
+	static int minus(int a, int b) {
+		return a-b;
+	}
+	
+}
